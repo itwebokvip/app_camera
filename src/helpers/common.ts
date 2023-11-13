c20e07a3e2aa7eeacf5ff54df425f693c74ae3b6
Jax Hoang <huynhhq209@gmail.com>
2023-11-13 16:22:25 +0700

    fix: refactor code

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -2,4 +2,4 @@ export const Sleep = (second: number): Promise<void> =>
   new Promise(resolve => setTimeout(resolve, second * 1000));
 
 export const IMAGE_DOMAIN = 'https://api-camera.okvip.dev/upload';
-export const GOOGLE_MAP_API_KEY = 'AIzaSyDLhd9XefFpeUg4IElB8o61-bltlA-oSzo';
+export const GOOGLE_MAP_API_KEY = 'AIzaSyA21JwqECJSJuIoHQ4nDZEaKI8Ol9KoDbg';
